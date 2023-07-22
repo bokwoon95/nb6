543c896fdc44815f931573850bd113bcab07bb63
bokwoon <bokwoon.c@gmail.com>
2023-07-22 16:47:11 +0800

    checkpoint

--- static.go
+++ static.go
@@ -25,6 +25,7 @@ func (nbrew *Notebrew) static(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	// Path: "/admin/static/styles.css" => name: "static/styles.css"
 	_, name, _ := strings.Cut(strings.Trim(r.URL.Path, "/"), "/")
 	head, _, _ := strings.Cut(name, "/")
 	if head != "static" {
@@ -70,10 +71,6 @@ func (nbrew *Notebrew) static(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	multiWriter := io.MultiWriter(buf, hash)
-	// TODO: we determine the file extension here and set the Content-Type
-	// ourselves directly instead of relying on http.ServeContent to do so for
-	// us. Then we can pass in an empty name for the string (no need to
-	// TrimSuffix ".gz") and we can pass in gzipped bytes to the bytes.Reader.
 	if strings.HasSuffix(name, ".gz") {
 		_, err = io.Copy(multiWriter, file)
 		if err != nil {
