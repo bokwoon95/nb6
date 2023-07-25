7d3905ee46c6ef9e6b5c477b58492e530ca426b8
bokwoon <bokwoon.c@gmail.com>
2023-07-26 03:25:31 +0800

    checkpoint

--- delete.go
+++ delete.go
@@ -1,1 +1,162 @@
 package nb6
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"html/template"
+	"io/fs"
+	"mime"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+
+	"golang.org/x/exp/slog"
+)
+
+func (nbrew *Notebrew) delet(w http.ResponseWriter, r *http.Request) {
+	type Request struct {
+		Path string `json:"path,omitempty"`
+	}
+	type Response struct {
+		Path   string     `json:"path,omitempty"`
+		Errors url.Values `json:"errors,omitempty"`
+	}
+
+	logger, ok := r.Context().Value(loggerKey).(*slog.Logger)
+	if !ok {
+		logger = slog.Default()
+	}
+
+	var sitePrefix string
+	segments := strings.Split(strings.Trim(r.URL.Path, "/"), "/")
+	if len(segments) > 1 && (strings.HasPrefix(segments[1], "@") || strings.Contains(segments[1], ".")) {
+		sitePrefix = segments[1]
+	}
+
+	switch r.Method {
+	case "GET":
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, fmt.Sprintf("400 Bad Request: %s", err), http.StatusBadRequest)
+			return
+		}
+		var response Response
+		ok, err := nbrew.getSession(r, "flash", &response)
+		if err != nil {
+			logger.Error(err.Error())
+		} else if !ok {
+			response.Path = r.Form.Get("path")
+		}
+		if response.Path != "" {
+			response.Path = strings.Trim(path.Clean(response.Path), "/")
+		}
+		nbrew.clearSession(w, r, "flash")
+
+		tmpl, err := template.ParseFS(rootFS, "html/delete.html")
+		if err != nil {
+			logger.Error(err.Error())
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		buf := bufPool.Get().(*bytes.Buffer)
+		buf.Reset()
+		defer bufPool.Put(buf)
+		err = tmpl.Execute(buf, &response)
+		if err != nil {
+			logger.Error(err.Error())
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		buf.WriteTo(w)
+	case "POST":
+		var request Request
+		contentType, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
+		switch contentType {
+		case "application/json":
+			err := json.NewDecoder(r.Body).Decode(&request)
+			if err != nil {
+				var syntaxErr *json.SyntaxError
+				if errors.As(err, &syntaxErr) {
+					http.Error(w, "400 Bad Request: invalid JSON", http.StatusBadRequest)
+					return
+				}
+				logger.Error(err.Error())
+				http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+		case "application/x-www-form-urlencoded":
+			err := r.ParseForm()
+			if err != nil {
+				http.Error(w, fmt.Sprintf("400 Bad Request: %s", err), http.StatusBadRequest)
+				return
+			}
+			request.Path = r.Form.Get("path")
+		default:
+			http.Error(w, "415 Unsupported Media Type", http.StatusUnsupportedMediaType)
+			return
+		}
+
+		writeResponse := func(w http.ResponseWriter, r *http.Request, response Response) {
+			accept, _, _ := mime.ParseMediaType(r.Header.Get("Accept"))
+			if accept == "application/json" {
+				b, err := json.Marshal(&response)
+				if err != nil {
+					logger.Error(err.Error())
+					http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+					return
+				}
+				w.Write(b)
+				return
+			}
+			if len(response.Errors) > 0 {
+				err := nbrew.setSession(w, r, &response, &http.Cookie{
+					Path:     r.URL.Path,
+					Name:     "flash",
+					Secure:   nbrew.Scheme == "https://",
+					HttpOnly: true,
+					SameSite: http.SameSiteLaxMode,
+				})
+				if err != nil {
+					logger.Error(err.Error())
+					http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+					return
+				}
+				http.Redirect(w, r, r.URL.String(), http.StatusFound)
+				return
+			}
+			http.Redirect(w, r, nbrew.Scheme+nbrew.AdminDomain+"/"+path.Join("admin", sitePrefix, path.Dir(response.Path))+"/", http.StatusFound)
+		}
+
+		response := Response{
+			Path: request.Path,
+		}
+		if response.Path != "" {
+			response.Path = strings.Trim(path.Clean(response.Path), "/")
+		}
+
+		_, err := fs.Stat(nbrew.FS, path.Join(sitePrefix, response.Path))
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				response.Errors.Add("path", "file or folder does not exist")
+				writeResponse(w, r, response)
+				return
+			}
+			logger.Error(err.Error())
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		err = nbrew.FS.Remove(path.Join(sitePrefix, response.Path))
+		if err != nil {
+			logger.Error(err.Error())
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		writeResponse(w, r, response)
+	default:
+		http.Error(w, "405 Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+}
