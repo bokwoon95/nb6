3c8c5ba627cf68c3395d78e9a9369ec7f5d62ee9
bokwoon <bokwoon.c@gmail.com>
2023-07-31 16:56:16 +0800

    checkpoint

--- go.mod
+++ go.mod
@@ -6,17 +6,29 @@ require (
 	github.com/bokwoon95/sq v0.3.5
 	github.com/bokwoon95/sqddl v0.4.5
 	github.com/caddyserver/certmagic v0.19.1
+	github.com/denisenkom/go-mssqldb v0.12.3
+	github.com/go-sql-driver/mysql v1.7.1
 	github.com/google/go-cmp v0.5.9
+	github.com/lib/pq v1.10.9
+	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/oklog/ulid/v2 v2.1.0
 	golang.org/x/crypto v0.11.0
 	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1
+	modernc.org/sqlite v1.24.0
 )
 
 require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/libdns/libdns v0.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mholt/acmez v1.2.0 // indirect
 	github.com/miekg/dns v1.1.55 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/zeebo/blake3 v0.2.3 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
@@ -27,4 +39,13 @@ require (
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 	golang.org/x/tools v0.10.0 // indirect
+	lukechampine.com/uint128 v1.2.0 // indirect
+	modernc.org/cc/v3 v3.40.0 // indirect
+	modernc.org/ccgo/v3 v3.16.13 // indirect
+	modernc.org/libc v1.22.5 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.5.0 // indirect
+	modernc.org/opt v0.1.3 // indirect
+	modernc.org/strutil v1.1.3 // indirect
+	modernc.org/token v1.0.1 // indirect
 )
--- notebrew/main.go
+++ notebrew/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"flag"
+	"fmt"
+	"net"
+	"net/http"
 	"os"
+	"os/signal"
 	"path/filepath"
+	"runtime"
 	"strings"
+	"syscall"
+	"time"
+
+	"github.com/bokwoon95/nb6"
 )
 
 var open = func(address string) {}
 
 func main() {
-	// notebrew -dir -addr -multisite subdirectory
-	var dir, addr, db string
+	// notebrew -dir -addr -multisite -db
+	var dir, addr, multisite, db string
 	flagset := flag.NewFlagSet("", flag.ContinueOnError)
 	flagset.StringVar(&dir, "dir", "", "")
 	flagset.StringVar(&addr, "addr", "", "")
+	flagset.StringVar(&multisite, "multisite", "", "")
 	flagset.StringVar(&db, "db", "", "")
 	err := flagset.Parse(os.Args[1:])
 	if err != nil {
@@ -43,6 +54,13 @@ func main() {
 			exit(err)
 		}
 	}
+	multisite = strings.TrimSpace(multisite)
+	if multisite != "" {
+		err = os.WriteFile(filepath.Join(dir, "multisite.txt"), []byte(multisite), 0644)
+		if err != nil {
+			exit(err)
+		}
+	}
 	db = strings.TrimSpace(db)
 	if db != "" {
 		err = os.WriteFile(filepath.Join(dir, "database.txt"), []byte(db), 0644)
@@ -50,4 +68,74 @@ func main() {
 			exit(err)
 		}
 	}
+	nbrew, err := nb6.New(&nb6.LocalFS{RootDir: dir})
+	if err != nil {
+		exit(err)
+	}
+	defer nbrew.Close()
+	args := flagset.Args()
+	if len(args) > 0 {
+		if nbrew.DB == nil {
+			err = os.WriteFile(filepath.Join(dir, "database.txt"), []byte("sqlite"), 0644)
+			if err != nil {
+				exit(err)
+			}
+			nbrew, err = nb6.New(&nb6.LocalFS{RootDir: dir})
+			if err != nil {
+				exit(err)
+			}
+		}
+		return
+	}
+	server, err := nbrew.NewServer()
+	if err != nil {
+		exit(err)
+	}
+	wait := make(chan os.Signal, 1)
+	signal.Notify(wait, syscall.SIGINT, syscall.SIGTERM)
+	if nbrew.Protocol == "https://" {
+		go http.ListenAndServe(":80", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != "GET" && r.Method != "HEAD" {
+				http.Error(w, "Use HTTPS", http.StatusBadRequest)
+				return
+			}
+			host, _, err := net.SplitHostPort(r.Host)
+			if err != nil {
+				host = r.Host
+			} else {
+				host = net.JoinHostPort(host, "443")
+			}
+			http.Redirect(w, r, "https://"+host+r.URL.RequestURI(), http.StatusFound)
+		}))
+		fmt.Println("Listening on " + server.Addr)
+		go server.ListenAndServeTLS("", "")
+	} else {
+		listener, err := net.Listen("tcp", server.Addr)
+		if err != nil {
+			var errno syscall.Errno
+			if !errors.As(err, &errno) {
+				exit(err)
+			}
+			// WSAEADDRINUSE copied from
+			// https://cs.opensource.google/go/x/sys/+/refs/tags/v0.6.0:windows/zerrors_windows.go;l=2680
+			// To avoid importing an entire 3rd party library just to use a constant.
+			const WSAEADDRINUSE = syscall.Errno(10048)
+			if errno == syscall.EADDRINUSE || runtime.GOOS == "windows" && errno == WSAEADDRINUSE {
+				fmt.Println("http://" + server.Addr)
+				open("http://" + server.Addr)
+			}
+			return
+		}
+		open("http://" + server.Addr)
+		// NOTE: We may need to give a more intricate ASCII header in order for the
+		// GUI double clickers to realize that the terminal window is important, so
+		// that they won't accidentally close it thinking it is some random
+		// terminal.
+		fmt.Println("Listening on http://" + server.Addr)
+		go server.Serve(listener)
+	}
+	<-wait
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	server.Shutdown(ctx)
 }
