188841640592057dee471238e22721351e27d475
bokwoon <bokwoon.c@gmail.com>
2023-07-31 13:53:55 +0800

    checkpoint

--- new.go
+++ new.go
@@ -19,7 +19,6 @@ import (
 	"time"
 
 	"github.com/caddyserver/certmagic"
-	"golang.org/x/exp/slices"
 	"golang.org/x/exp/slog"
 )
 
@@ -28,29 +27,113 @@ func New(fsys FS) (*Notebrew, error) {
 		FS:        fsys,
 		ErrorCode: func(error) string { return "" },
 	}
-	adminDir, err := filepath.Abs(fmt.Sprint(nbrew.FS))
+	localDir, err := filepath.Abs(fmt.Sprint(nbrew.FS))
 	if err == nil {
-		fileInfo, err := os.Stat(adminDir)
+		fileInfo, err := os.Stat(localDir)
 		if err != nil || !fileInfo.IsDir() {
-			adminDir = ""
+			localDir = ""
 		}
 	}
-	nbrew.Protocol, nbrew.AdminDomain, nbrew.ContentDomain, err = getDomains(nbrew.FS, adminDir)
+
+	// Read from address.txt.
+	b, err := fs.ReadFile(nbrew.FS, "address.txt")
 	if err != nil {
-		return nil, err
+		if !errors.Is(err, fs.ErrNotExist) {
+			return nil, fmt.Errorf("%s: %v", filepath.Join(localDir, "address.txt"), err)
+		}
+		nbrew.Protocol = "http://"
+		nbrew.AdminDomain = "localhost:6444"
+		nbrew.ContentDomain = "localhost:6444"
+	} else {
+		address := strings.TrimSpace(string(b))
+		if address == "" {
+			nbrew.Protocol = "http://"
+			nbrew.AdminDomain = "localhost:6444"
+			nbrew.ContentDomain = "localhost:6444"
+		} else if strings.HasPrefix(address, ":") {
+			_, err = strconv.Atoi(address[1:])
+			if err != nil {
+				return nil, fmt.Errorf(
+					"%s: %q is not a valid port (must be a number e.g. :6444)",
+					filepath.Join(localDir, "address.txt"),
+					address,
+				)
+			}
+			nbrew.Protocol = "http://"
+			nbrew.AdminDomain = "localhost" + address
+			nbrew.ContentDomain = "localhost" + address
+		} else {
+			nbrew.Protocol = "https://"
+			lines := strings.Split(address, "\n")
+			if len(lines) == 1 {
+				nbrew.AdminDomain = strings.TrimSpace(lines[0])
+				nbrew.ContentDomain = strings.TrimSpace(lines[0])
+			} else if len(lines) == 2 {
+				nbrew.AdminDomain = strings.TrimSpace(lines[0])
+				nbrew.ContentDomain = strings.TrimSpace(lines[1])
+			} else {
+				return nil, fmt.Errorf("%s contains too many lines, maximum 2 lines." +
+					" The first line is the admin domain, the second line is the content domain." +
+					" Alternatively, if only one line is provided it will be used as as both the admin domain and content domain." +
+					filepath.Join(localDir, "address.txt"),
+				)
+			}
+			if !strings.Contains(nbrew.AdminDomain, ".") {
+				return nil, fmt.Errorf("%s: %q is not a valid domain (e.g. example.com):"+
+					" missing a top level domain (.com, .org, .net, etc)",
+					filepath.Join(localDir, "address.txt"),
+					nbrew.AdminDomain,
+				)
+			}
+			for _, char := range nbrew.AdminDomain {
+				if (char >= '0' && char <= '9') || (char >= 'a' && char <= 'z') || char == '.' || char == '-' {
+					continue
+				}
+				return nil, fmt.Errorf("%s: %q is not a valid domain (e.g. example.com):"+
+					" only lowercase letters, numbers, dot and hyphen are allowed",
+					filepath.Join(localDir, "address.txt"),
+					nbrew.AdminDomain,
+				)
+			}
+			if !strings.Contains(nbrew.ContentDomain, ".") {
+				return nil, fmt.Errorf("%s: %q is not a valid domain (e.g. example.com):"+
+					" missing a top level domain (.com, .org, .net, etc)",
+					filepath.Join(localDir, "address.txt"),
+					nbrew.ContentDomain,
+				)
+			}
+			for _, char := range nbrew.ContentDomain {
+				if (char >= '0' && char <= '9') || (char >= 'a' && char <= 'z') || char == '.' || char == '-' {
+					continue
+				}
+				return nil, fmt.Errorf("%s: %q is not a valid domain (e.g. example.com):"+
+					" only lowercase letters, numbers, dot and hyphen are allowed",
+					filepath.Join(localDir, "address.txt"),
+					nbrew.ContentDomain,
+				)
+			}
+		}
 	}
-	nbrew.MultisiteMode, err = getMultisiteMode(nbrew.FS, adminDir)
+
+	// Read from multisite.txt.
+	b, err = fs.ReadFile(nbrew.FS, "multisite.txt")
 	if err != nil {
-		return nil, err
+		if !errors.Is(err, fs.ErrNotExist) {
+			return nil, fmt.Errorf("%s: %v", filepath.Join(localDir, "multisite.txt"), err)
+		}
+	} else {
+		nbrew.MultisiteMode = strings.ToLower(string(b))
+	}
+	if nbrew.MultisiteMode != "" && nbrew.MultisiteMode != "subdomain" && nbrew.MultisiteMode != "subdirectory" {
+		return nil, fmt.Errorf(
+			`%s: %q is not a valid multisite value (accepted values: "", "subdomain", "subdirectory")`,
+			filepath.Join(localDir, "multisite.txt"),
+			nbrew.MultisiteMode,
+		)
 	}
 
 	// Read from database.txt.
-	var driverName string
-	var dataSourceName string
-	nbrew.Dialect, driverName, dataSourceName, err = getDataSourceName(nbrew.FS, adminDir, nbrew.Protocol)
-	if err != nil {
-		return nil, err
-	}
+	var dsn string
 	b, err = fs.ReadFile(nbrew.FS, "database.txt")
 	if err != nil {
 		if !errors.Is(err, fs.ErrNotExist) {
@@ -60,91 +143,89 @@ func New(fsys FS) (*Notebrew, error) {
 			// If database.txt doesn't exist but we are serving a live site, we
 			// have to create a database. In this case, fall back to an SQLite
 			// database.
-			nbrew.Dialect = "sqlite"
-			driverName = "sqlite3"
-			dataSourceName = filepath.Join(adminDir, "notebrew.db")
+			dsn = "sqlite"
 		}
 	} else {
-		dataSourceName = strings.TrimSpace(string(b))
-		if strings.HasPrefix(dataSourceName, "file:") {
-			filename, _, _ := strings.Cut(strings.TrimPrefix(strings.TrimPrefix(dataSourceName, "file:"), "//"), "?")
+		dsn = strings.TrimSpace(string(b))
+		if strings.HasPrefix(dsn, "file:") {
+			filename := strings.TrimPrefix(strings.TrimPrefix(dsn, "file:"), "//")
 			file, err := os.Open(filename)
 			if err != nil {
-				if errors.Is(err, fs.ErrNotExist) && slices.Contains([]string{".sqlite", ".sqlite3", ".db", ".db3"}, filepath.Ext(filename)) {
-					nbrew.Dialect = "sqlite"
-					driverName = "sqlite3"
-					dataSourceName = filepath.Join(adminDir, "notebrew.db")
+				ext := filepath.Ext(filename)
+				if errors.Is(err, fs.ErrNotExist) && (ext == ".sqlite" || ext == ".sqlite3" || ext == ".db" || ext == ".db3") {
+					dsn = filename
 				} else {
-					return nil, fmt.Errorf("%s: opening %q: %v", filepath.Join(adminDir, "database.txt"), dataSourceName, err)
+					return nil, fmt.Errorf("%s: opening %q: %v", filepath.Join(localDir, "database.txt"), dsn, err)
 				}
 			} else {
 				defer file.Close()
 				r := bufio.NewReader(file)
-				// SQLite databases may also start with a 'file:' prefix. Treat the
-				// contents of the file as a dsn only if the file isn't already an
-				// SQLite database i.e. the first 16 bytes isn't the SQLite file
-				// header. https://www.sqlite.org/fileformat.html#the_database_header
+				// SQLite databases may also start with a 'file:' prefix. Treat
+				// the contents of the file as a dsn only if the file isn't
+				// already an SQLite database i.e. the first 16 bytes isn't the
+				// SQLite file header.
+				// https://www.sqlite.org/fileformat.html#the_database_header
 				header, err := r.Peek(16)
 				if err != nil {
-					return nil, fmt.Errorf("%s: reading %q: %v", filepath.Join(adminDir, "database.txt"), dataSourceName, err)
+					return nil, fmt.Errorf("%s: reading %q: %v", filepath.Join(localDir, "database.txt"), dsn, err)
 				}
 				if string(header) == "SQLite format 3\x00" {
-					dataSourceName = "sqlite:" + dataSourceName
+					dsn = "sqlite:" + filename
 				} else {
 					var b strings.Builder
 					_, err = r.WriteTo(&b)
 					if err != nil {
-						return "", "", ""
+						return nil, fmt.Errorf("%s: reading %q: %v", filepath.Join(localDir, "database.txt"), dsn, err)
 					}
-					dataSourceName = strings.TrimSpace(b.String())
+					dsn = strings.TrimSpace(b.String())
 				}
 			}
 		}
+	}
+	if dsn != "" {
 		// Determine the database dialect from the dsn.
-		if dataSourceName == "sqlite" {
+		if dsn == "sqlite" {
 			nbrew.Dialect = "sqlite"
-		} else if strings.HasPrefix(dataSourceName, "postgres://") {
+			if localDir == "" {
+				return nil, fmt.Errorf("unable to create sqlite database")
+			}
+			dsn = filepath.Join(localDir, "notebrew.db")
+		} else if strings.HasPrefix(dsn, "sqlite:") || strings.HasPrefix(dsn, "sqlite3:") {
+			nbrew.Dialect = "sqlite"
+		} else if strings.HasPrefix(dsn, "postgres://") {
 			nbrew.Dialect = "postgres"
-		} else if strings.HasPrefix(dataSourceName, "mysql://") {
+		} else if strings.HasPrefix(dsn, "mysql://") {
 			nbrew.Dialect = "mysql"
-		} else if strings.HasPrefix(dataSourceName, "sqlserver://") {
+		} else if strings.HasPrefix(dsn, "sqlserver://") {
 			nbrew.Dialect = "sqlserver"
-		} else if strings.Contains(dataSourceName, "@tcp(") || strings.Contains(dataSourceName, "@unix(") {
+		} else if strings.Contains(dsn, "@tcp(") || strings.Contains(dsn, "@unix(") {
 			nbrew.Dialect = "mysql"
-		} else if dataSourceName != "" {
-			return nil, fmt.Errorf("database.txt: unknown dsn %q", dataSourceName)
+		} else {
+			ext := filepath.Ext(dsn)
+			if ext == ".sqlite" || ext == ".sqlite3" || ext == ".db" || ext == ".db3" {
+				nbrew.Dialect = "sqlite"
+			} else {
+				return nil, fmt.Errorf("%s: unknown or unsupported dataSourceName %q", filepath.Join(localDir, "database.txt"), dsn)
+			}
 		}
-	}
-	if nbrew.Dialect == "sqlserver" {
-		// NOTE: not supporting sqlserver until I can insert NULL (VAR)BINARY
-		// values into the database
-		// (https://github.com/denisenkom/go-mssqldb/issues/196).
-		return nil, fmt.Errorf("database.txt: sqlserver is not supported")
-	}
-	if nbrew.Dialect == "sqlite" {
-		// SQLite databases can only be created by giving it a filepath on the
-		// current system. Check if we can convert srv.fs into a filepath
-		// string, then check if it is a valid directory. If it is, we can
-		// create an SQLite database there.
-		if adminDir == "" {
-			return nil, fmt.Errorf("unable to create DB")
-		}
-	}
-	if dataSourceName != "" {
-		var driverName string
 		// Set a default driverName depending on the dialect.
+		var driverName string
 		switch nbrew.Dialect {
 		case "sqlite":
+			// Assumes driver will be github.com/mattn/go-sqlite3.
 			driverName = "sqlite3"
 		case "postgres":
+			// Assumes driver will be github.com/lib/pq.
 			driverName = "postgres"
 		case "mysql":
+			// Assumes driver will be github.com/go-sql-driver/mysql.
 			driverName = "mysql"
 		case "sqlserver":
+			// Assumes driver will be github.com/denisenkom/go-mssqldb.
 			driverName = "sqlserver"
 		}
-		// Check if the user registered any driverName/dsn overrides for the
-		// dialect.
+		// Check if the user registered any driverName/dsn overrides
+		// for the dialect.
 		dbDriversMu.RLock()
 		d := dbDrivers[nbrew.Dialect]
 		dbDriversMu.RUnlock()
@@ -152,26 +233,32 @@ func New(fsys FS) (*Notebrew, error) {
 			driverName = d.DriverName
 		}
 		if d.PreprocessDSN != nil {
-			dataSourceName, err = d.PreprocessDSN(dataSourceName)
+			dsn, err = d.PreprocessDSN(dsn)
 			if err != nil {
 				return nil, err
 			}
 		} else {
-			if nbrew.Dialect == "sqlite" {
-				if strings.HasPrefix(dataSourceName, "sqlite3:") {
-					dataSourceName = strings.TrimPrefix(strings.TrimPrefix(dataSourceName, "sqlite3:"), "//")
-				} else if strings.HasPrefix(dataSourceName, "sqlite:") {
-					dataSourceName = strings.TrimPrefix(strings.TrimPrefix(dataSourceName, "sqlite:"), "//")
+			// Do some default dsn cleaning if no custom dialect Driver was
+			// registered. We assume the default drivers of
+			// "github.com/mattn/go-sqlite3" and
+			// "github.com/go-sql-driver/mysql", which don't accept "sqlite:"
+			// or "mysql://" prefixes so trim that away.
+			switch nbrew.Dialect {
+			case "sqlite":
+				if strings.HasPrefix(dsn, "sqlite3:") {
+					dsn = strings.TrimPrefix(strings.TrimPrefix(dsn, "sqlite3:"), "//")
+				} else if strings.HasPrefix(dsn, "sqlite:") {
+					dsn = strings.TrimPrefix(strings.TrimPrefix(dsn, "sqlite:"), "//")
 				}
-			} else if nbrew.Dialect == "mysql" {
-				dataSourceName = strings.TrimPrefix(dataSourceName, "mysql://")
+			case "mysql":
+				dsn = strings.TrimPrefix(dsn, "mysql://")
 			}
 		}
 		if d.ErrorCode != nil {
 			nbrew.ErrorCode = d.ErrorCode
 		}
 		// Open the database using the driverName and dsn.
-		nbrew.DB, err = sql.Open(driverName, dataSourceName)
+		nbrew.DB, err = sql.Open(driverName, dsn)
 		if err != nil {
 			return nil, err
 		}
@@ -260,193 +347,6 @@ func (nbrew *Notebrew) NewServer() (*http.Server, error) {
 		server.TLSConfig.NextProtos = []string{"h2", "http/1.1", "acme-tls/1"}
 	}
 	return server, nil
-}
-
-func getDomains(fsys fs.FS, fsysName string) (scheme, adminDomain, contentDomain string, err error) {
-	b, err := fs.ReadFile(fsys, "address.txt")
-	if err != nil {
-		if errors.Is(err, fs.ErrNotExist) {
-			return "http://", "localhost:6444", "localhost:6444", nil
-		}
-		return "", "", "", fmt.Errorf("%s: %v", filepath.Join(fsysName, "address.txt"), err)
-	}
-	address := strings.TrimSpace(string(b))
-	if address == "" {
-		return "http://", "localhost:6444", "localhost:6444", nil
-	}
-	if strings.HasPrefix(address, ":") {
-		_, err = strconv.Atoi(address[1:])
-		if err != nil {
-			return "", "", "", fmt.Errorf(
-				"%s: %q is not a valid port (must be a number e.g. :6444)",
-				filepath.Join(fsysName, "address.txt"),
-				address,
-			)
-		}
-		return "http://", "localhost:6444", "localhost:6444", nil
-	}
-	lines := strings.Split(address, "\n")
-	if len(lines) == 1 {
-		adminDomain = strings.TrimSpace(lines[0])
-		contentDomain = strings.TrimSpace(lines[0])
-	} else if len(lines) == 2 {
-		adminDomain = strings.TrimSpace(lines[0])
-		contentDomain = strings.TrimSpace(lines[1])
-	} else {
-		return "", "", "", fmt.Errorf("%s contains too many lines, maximum 2 lines." +
-			" The first line is the admin domain, the second line is the content domain." +
-			" Alternatively, if only one line is provided it will be used as as both the admin domain and content domain." +
-			filepath.Join(fsysName, "address.txt"),
-		)
-	}
-	if !strings.Contains(adminDomain, ".") {
-		return "", "", "", fmt.Errorf("%s: %q is not a valid domain (e.g. example.com):"+
-			" missing a top level domain (.com, .org, .net, etc)",
-			filepath.Join(fsysName, "address.txt"),
-			adminDomain,
-		)
-	}
-	for _, char := range adminDomain {
-		if (char >= '0' && char <= '9') || (char >= 'a' && char <= 'z') || char == '.' || char == '-' {
-			continue
-		}
-		return "", "", "", fmt.Errorf("%s: %q is not a valid domain (e.g. example.com):"+
-			" only lowercase letters, numbers, dot and hyphen are allowed",
-			filepath.Join(fsysName, "address.txt"),
-			adminDomain,
-		)
-	}
-	if !strings.Contains(contentDomain, ".") {
-		return "", "", "", fmt.Errorf("%s: %q is not a valid domain (e.g. example.com):"+
-			" missing a top level domain (.com, .org, .net, etc)",
-			filepath.Join(fsysName, "address.txt"),
-			contentDomain,
-		)
-	}
-	for _, char := range contentDomain {
-		if (char >= '0' && char <= '9') || (char >= 'a' && char <= 'z') || char == '.' || char == '-' {
-			continue
-		}
-		return "", "", "", fmt.Errorf("%s: %q is not a valid domain (e.g. example.com):"+
-			" only lowercase letters, numbers, dot and hyphen are allowed",
-			filepath.Join(fsysName, "address.txt"),
-			contentDomain,
-		)
-	}
-	return "https://", adminDomain, contentDomain, nil
-}
-
-// TODO: if fsysName is purely for reporting purposes, we can defer the
-// embedding of the full file path to the caller instead.
-func getMultisiteMode(fsys fs.FS, fsysName string) (multisiteMode string, err error) {
-	b, err := fs.ReadFile(fsys, "multisite.txt")
-	if err != nil {
-		if errors.Is(err, fs.ErrNotExist) {
-			return "", nil
-		}
-		return "", fmt.Errorf("%s: %v", filepath.Join(fsysName, "multisite.txt"), err)
-	}
-	multisiteMode = strings.ToLower(string(b))
-	if multisiteMode != "" && multisiteMode != "subdomain" && multisiteMode != "subdirectory" {
-		return "", fmt.Errorf(
-			`%s: %q is not a valid multisite value (accepted values: "", "subdomain", "subdirectory")`,
-			filepath.Join(fsysName, "multisite.txt"),
-			multisiteMode,
-		)
-	}
-	return multisiteMode, nil
-}
-
-// TODO: FUHG I still need to get the ErrorCode from the driver. Revamp this function's signature to accept a *Notebrew perhaps?
-func getDataSourceName(fsys fs.FS, fsysName string, protocol string) (dialect string, driverName string, dataSourceName string, err error) {
-	b, err := fs.ReadFile(fsys, "database.txt")
-	if err != nil {
-		if errors.Is(err, fs.ErrNotExist) {
-			if protocol == "https://" {
-				return "sqlite", "sqlite3", filepath.Join(fsysName, "notebrew.db"), nil
-			}
-			return "", "", "", nil
-		}
-		return "", "", "", fmt.Errorf("%s: %v", filepath.Join(fsysName, "database.txt"), err)
-	}
-	dataSourceName = strings.TrimSpace(string(b))
-	if strings.HasPrefix(dataSourceName, "file:") {
-		filename, _, _ := strings.Cut(strings.TrimPrefix(strings.TrimPrefix(dataSourceName, "file:"), "//"), "?")
-		file, err := os.Open(filename)
-		if err != nil {
-			ext := filepath.Ext(filename)
-			if errors.Is(err, fs.ErrNotExist) && (ext == ".sqlite" || ext == ".sqlite3" || ext == ".db" || ext == ".db3") {
-				return "sqlite", "sqlite3", filename, nil
-			}
-			return "", "", "", fmt.Errorf("%s: opening %q: %v", filepath.Join(fsysName, "database.txt"), dataSourceName, err)
-		} else {
-			defer file.Close()
-			r := bufio.NewReader(file)
-			// SQLite databases may also start with a 'file:' prefix. Treat the
-			// contents of the file as a dsn only if the file isn't already an
-			// SQLite database i.e. the first 16 bytes isn't the SQLite file
-			// header. https://www.sqlite.org/fileformat.html#the_database_header
-			header, err := r.Peek(16)
-			if err != nil {
-				return "", "", "", fmt.Errorf("%s: reading %q: %v", filepath.Join(fsysName, "database.txt"), dataSourceName, err)
-			}
-			if string(header) == "SQLite format 3\x00" {
-				dataSourceName = "sqlite:" + dataSourceName
-			} else {
-				var b strings.Builder
-				_, err = r.WriteTo(&b)
-				if err != nil {
-					return "", "", "", fmt.Errorf("%s: reading %q: %v", filepath.Join(fsysName, "database.txt"), dataSourceName, err)
-				}
-				dataSourceName = strings.TrimSpace(b.String())
-			}
-		}
-	}
-	if dataSourceName == "sqlite" {
-		dialect = "sqlite"
-		dataSourceName = filepath.Join(fsysName, "notebrew.db")
-	} else if strings.HasPrefix(dataSourceName, "postgres://") {
-		dialect = "postgres"
-	} else if strings.HasPrefix(dataSourceName, "mysql://") {
-		dialect = "mysql"
-	} else {
-		dsn, _, _ := strings.Cut(dataSourceName, "?")
-		ext := filepath.Ext(dsn)
-		if ext == ".sqlite" || ext == ".sqlite3" || ext == ".db" || ext == ".db3" {
-			dialect = "sqlite"
-		} else {
-			return "", "", "", fmt.Errorf("%s: unknown or unsupported dataSourceName %q", filepath.Join(fsysName, "database.txt"), dataSourceName)
-		}
-	}
-	dbDriversMu.RLock()
-	driver, ok := dbDrivers[dialect]
-	dbDriversMu.RUnlock()
-	if ok {
-		driverName = driver.DriverName
-		if driver.PreprocessDSN != nil {
-			newDataSourceName, err := driver.PreprocessDSN(dataSourceName)
-			if err != nil {
-				return "", "", "", fmt.Errorf("%s: error occurred when trying to preprocess %q: %v", filepath.Join(fsysName, "database.txt"), dataSourceName, err)
-			}
-			return dialect, driver.DriverName, newDataSourceName, nil
-		}
-		return dialect, driver.DriverName, dataSourceName, nil
-	}
-	switch dialect {
-	case "sqlite":
-		var tmp string
-		if strings.HasPrefix(dataSourceName, "sqlite3:") {
-			tmp = strings.TrimPrefix(dataSourceName, "sqlite3:")
-		} else {
-			tmp = strings.TrimPrefix(dataSourceName, "sqlite:")
-		}
-		return dialect, "sqlite3", strings.TrimPrefix(tmp, "//"), nil
-	case "postgres":
-		return dialect, "postgres", dataSourceName, nil
-	case "mysql":
-		return dialect, "mysql", strings.TrimPrefix(dataSourceName, "mysql://"), nil
-	}
-	return "", "", "", fmt.Errorf("%s: unknown or unsupported dataSourceName %q", filepath.Join(fsysName, "database.txt"), dataSourceName)
 }
 
 var (
