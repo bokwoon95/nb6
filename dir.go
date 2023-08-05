bf07923ff5b81dce08677909f40946cf94480dcd
bokwoon <bokwoon.c@gmail.com>
2023-08-06 01:15:41 +0800

    checkpoint

--- dir.go
+++ dir.go
@@ -3,6 +3,7 @@ package nb6
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"html/template"
 	"io/fs"
 	"net/http"
@@ -15,11 +16,12 @@ import (
 
 func (nbrew *Notebrew) dir(w http.ResponseWriter, r *http.Request) {
 	type Entry struct {
-		Name    string     `json:"name,omitempty"`
-		IsDir   bool       `json:"is_dir,omitempty"`
-		Size    *int64     `json:"size,omitempty"`
-		ModTime *time.Time `json:"mod_time,omitempty"`
+		Name    string    `json:"name,omitempty"`
+		IsDir   bool      `json:"is_dir,omitempty"`
+		Size    int64     `json:"size,omitempty"`
+		ModTime time.Time `json:"mod_time,omitempty"`
 	}
+	// {{ sizeToUnits $entry.Size }}
 	type Response struct {
 		Path    string  `json:"path"`
 		Entries []Entry `json:"entries"`
@@ -31,10 +33,17 @@ func (nbrew *Notebrew) dir(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var funcMap = map[string]any{
-		"base": path.Base,
-		"join": path.Join,
+		"base":      path.Base,
+		"join":      path.Join,
+		"hasSuffix": strings.HasSuffix,
 		"generateBreadcrumbLinks": func(filePath string) template.HTML {
-			return template.HTML(filePath)
+			var b strings.Builder
+			b.WriteString(`<a href="/admin/" class="ma1">admin</a>`)
+			segments := strings.Split(strings.Trim(filePath, "/"), "/")
+			for i := 0; i < len(segments); i++ {
+				b.WriteString(fmt.Sprintf(`<a href="/admin/%s/" class="ma1">%s</a>`, path.Join(segments[:i+1]...), segments[i]))
+			}
+			return template.HTML(b.String())
 		},
 	}
 
@@ -93,4 +102,19 @@ func (nbrew *Notebrew) dir(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	buf.WriteTo(w)
+}
+
+func byteCountSI(size int64) string {
+	// https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
+	const unit = 1000
+	if size < unit {
+		return fmt.Sprintf("%d B", size)
+	}
+	div, exp := int64(unit), 0
+	for n := size / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB",
+		float64(size)/float64(div), "kMGTPE"[exp])
 }
--- notebrew.go
+++ notebrew.go
@@ -451,18 +451,3 @@ func (nbrew *Notebrew) IsForeignKeyViolation(err error) bool {
 		return false
 	}
 }
-
-func byteCountSI(b int64) string {
-	// https://yourbasic.org/golang/formatting-byte-size-to-human-readable-format/
-	const unit = 1000
-	if b < unit {
-		return fmt.Sprintf("%d B", b)
-	}
-	div, exp := int64(unit), 0
-	for n := b / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-	return fmt.Sprintf("%.1f %cB",
-		float64(b)/float64(div), "kMGTPE"[exp])
-}
