a78cdc0864d20bd5b976f14cb5ecb8af8d1495d7
bokwoon <bokwoon.c@gmail.com>
2023-09-01 01:27:00 +0800

    checkpoint

--- static/go-back.js
+++ static/go-back.js
@@ -1,7 +1,9 @@
 const element = document.querySelector("[data-go-back]");
 if (element && element.tagName == "A") {
     element.addEventListener("click", function(event) {
-        history.back();
-        event.preventDefault();
+        if (document.referrer) {
+            history.back();
+            event.preventDefault();
+        }
     });
 }
