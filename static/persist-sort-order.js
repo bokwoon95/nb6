510b662932cf004ea99c7c57a63fe5696fbcdb5d
bokwoon <bokwoon.c@gmail.com>
2023-09-07 05:05:55 +0800

    checkpoint

--- static/persist-sort-order.js
+++ static/persist-sort-order.js
@@ -1,29 +1,27 @@
 const urlSearchParams = (new URL(document.location)).searchParams;
 const suffix = " Path=" + location.pathname + "; Max-Age=" + (60 * 60 * 24 * 365).toString() + "; SameSite=Lax;";
-let sort = urlSearchParams.get("sort");
-if (sort) {
-    sort = sort.trim().toLowerCase();
+if (urlSearchParams.has("sort")) {
+    const sort = urlSearchParams.get("sort").trim().toLowerCase();
+    switch (sort) {
+        case "created":
+            document.cookie = "sort=created;" + suffix;
+            break;
+        case "edited":
+            document.cookie = "sort=edited;" + suffix;
+            break;
+        case "title":
+            document.cookie = "sort=title;" + suffix;
+            break;
+    }
 }
-switch (sort) {
-    case "created":
-        document.cookie = "sort=created;" + suffix;
-        break;
-    case "edited":
-        document.cookie = "sort=edited;" + suffix;
-        break;
-    case "title":
-        document.cookie = "sort=title;" + suffix;
-        break;
-}
-let order = urlSearchParams.get("order");
-if (order) {
-    order.trim().toLowerCase();
-}
-switch (order) {
-    case "asc":
-        document.cookie = "order=asc;" + suffix;
-        break;
-    case "desc":
-        document.cookie = "order=desc;" + suffix;
-        break;
+if (urlSearchParams.has("order")) {
+    const order = urlSearchParams.get("order").trim().toLowerCase();
+    switch (order) {
+        case "asc":
+            document.cookie = "order=asc;" + suffix;
+            break;
+        case "desc":
+            document.cookie = "order=desc;" + suffix;
+            break;
+    }
 }
