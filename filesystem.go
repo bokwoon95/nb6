d36f5256c2191b48f603b45f562f41b32b4f5c33
bokwoon <bokwoon.c@gmail.com>
2023-09-06 22:55:32 +0800

    checkpoint

--- filesystem.go
+++ filesystem.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"html/template"
 	"io/fs"
 	"mime"
@@ -251,6 +252,7 @@ func (nbrew *Notebrew) filesystem(w http.ResponseWriter, r *http.Request, userna
 		return
 	}
 	for _, dirEntry := range dirEntries {
+		fmt.Printf("dirEntry.Name: %q\n", dirEntry.Name())
 		entry := Entry{
 			Name:  dirEntry.Name(),
 			IsDir: dirEntry.IsDir(),
