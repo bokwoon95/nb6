8d998610cc311729ca153e0230916aa9793f45e7
bokwoon <bokwoon.c@gmail.com>
2023-09-07 13:46:03 +0800

    checkpoint

--- static/persist-sort-order.js
+++ static/persist-sort-order.js
@@ -1,4 +1,5 @@
 const urlSearchParams = (new URL(document.location)).searchParams;
+
 let sort = urlSearchParams.get("sort");
 if (sort) {
     sort = sort.trim().toLowerCase();
@@ -9,11 +10,12 @@ if (isDefaultSort) {
 } else if (sort === "name" || sort === "created" || sort === "edited" || sort === "title") {
     document.cookie = `sort=${sort}; Path=${location.pathname}; Max-Age=${60 * 60 * 24 * 365}; SameSite=Lax;`;
 }
+
 let order = urlSearchParams.get("order");
 if (order) {
     order = order.trim().toLowerCase();
 }
-const isDefaultOrder = ((sort === "title" || sort === "name") && order === "asc") || ((sort === "created" || sort === "edited") && order === "desc");
+const isDefaultOrder = order === null || ((sort === "title" || sort === "name") && order === "asc") || ((sort === "created" || sort === "edited") && order === "desc");
 if (isDefaultOrder) {
     document.cookie = `order=0; Path=${location.pathname}; Max-Age=-1; SameSite=Lax;`;
 } else if (order === "asc" || order === "desc") {
